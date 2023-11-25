f40cac4936ead69543e32a189586a155f223a4ff
Manoochehr Khatami <manouchehr.khatami@gmail.com>
2023-11-25 09:01:13 +0100

    import data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      Test
-    </>
-  )
+interface User {
+  id: number;
+  name: string;
 }
 
-export default App
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://mjsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
+  });
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
